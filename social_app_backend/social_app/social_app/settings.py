254c8c750782dda40e34a5f3de16ea5f604e270d
FWasik <80278631+FWasik@users.noreply.github.com>
2022-09-24 20:11:29 +0200

    backend - adding JWT

--- social_app_backend/social_app/social_app/settings.py
+++ social_app_backend/social_app/social_app/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
 }
 
@@ -130,3 +130,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+SIMPLE_JWT = {
+   'AUTH_HEADER_TYPES': ('JWT',),
+}
